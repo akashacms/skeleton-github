e59d37ab96a88752602839d014299c9684b07eb7
David Herron <david@davidherron.com>
2019-09-20 22:04:45 -0700

    Initial revision

---
+++ config.js
@@ -0,0 +1,75 @@
+
+
+'use strict';
+
+const akasha  = require('akasharender');
+
+const config = new akasha.Configuration();
+
+config.configDir = __dirname;
+
+config
+    .addAssetsDir('assets')
+    .addAssetsDir({
+        src: 'node_modules/bootstrap/dist',
+        dest: 'vendor/bootstrap'
+    })
+   .addAssetsDir({
+        src: 'node_modules/jquery/dist',
+        dest: 'vendor/jquery'
+    })
+    .addLayoutsDir('layouts')
+    .addDocumentsDir('documents')
+    .addPartialsDir('partials')
+    // This is for generating the site as Github Pages
+    .setRenderDestination('docs');
+
+config.rootURL("https://akashacms.github.io/skeleton-github/");
+
+config
+    .use(require('akashacms-theme-bootstrap'))
+    .use(require('akashacms-base'), {
+        generateSitemapFlag: true
+    })
+    .use(require('akashacms-breadcrumbs'))
+    .use(require('akashacms-booknav'))
+    .use(require('akashacms-embeddables'))
+    .use(require('akashacms-blog-podcast'), {
+        bloglist: {
+            news: {
+                rss: {
+                    title: "AkashaCMS Example Blog",
+                    description: "Skeleton blog for use with AkashaCMS",
+                    site_url: "http://blog-skeleton.akashacms.com/blog/index.html",
+                    image_url: "http://akashacms.com/logo.gif",
+                    managingEditor: 'David Herron',
+                    webMaster: 'David Herron',
+                    copyright: '2015 David Herron',
+                    language: 'en',
+                    categories: [ "Node.js", "Content Management System", "HTML5", "Static website generator" ]
+                },
+                rssurl: "/blog/rss.xml",
+                rootPath: "blog",
+                matchers: {
+                    layouts: [ "blog.html.ejs" ],
+                    path: /^blog\//
+                }
+            }
+        }
+    });
+
+config
+    .addFooterJavaScript({ href: "/vendor/jquery/jquery.min.js" })
+    .addFooterJavaScript({ href: "/vendor/bootstrap/js/bootstrap.min.js" })
+    .addStylesheet({ href: "/vendor/bootstrap/css/bootstrap.min.css" })
+    .addStylesheet({ href: "/vendor/bootstrap/css/bootstrap-theme.min.css" })
+    .addStylesheet({ href: "/style.css" });
+
+config.setMahabhutaConfig({
+    recognizeSelfClosing: true,
+    recognizeCDATA: true
+});
+
+config.prepare();
+
+module.exports = config;
---
+++ documents/index.html.md
@@ -0,0 +1,10 @@
+---
+layout: home-page.html.ejs
+title: Blog Home Page
+---
+
+
+<div id="blog-1-news">
+    <div class="well well-sm"><h2>Blog #1</h2></div>
+    <blog-news-river maxentries="20" blogtag="news" template="blog-river-thumbs.html.ejs"></blog-news-river>
+</div>
